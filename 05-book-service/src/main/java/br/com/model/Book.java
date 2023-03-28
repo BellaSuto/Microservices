bab74f6c3eeee8c3da39747b56684c9e3271845b
Bella <bellasuto2@gmail.com>
2023-03-28 15:31:20 -0300

    conectando microserviços

---
+++ 05-book-service/src/main/java/br/com/model/Book.java
@@ -0,0 +1,129 @@
+package br.com.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.sql.Timestamp;
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Book implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 180)
+    private String author;
+    @Column(name = "launch_date", nullable = false, length = 180)
+    private Timestamp launchDate;
+    private double price;
+    @Column(nullable = false)
+    private String title;
+    @Transient
+    private String currency;
+    @Transient
+    private String environment;
+
+    public Book() {
+    }
+
+    public Book(Long id, String author, Timestamp launchDate, double price, String title, String currency, String environment) {
+        this.id = id;
+        this.author = author;
+        this.launchDate = launchDate;
+        this.price = price;
+        this.title = title;
+        this.currency = currency;
+        this.environment = environment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Timestamp getLaunchDate() {
+        return launchDate;
+    }
+
+    public void setLaunchDate(Timestamp launchDate) {
+        this.launchDate = launchDate;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book book)) return false;
+
+        if (Double.compare(book.getPrice(), getPrice()) != 0) return false;
+        if (getId() != null ? !getId().equals(book.getId()) : book.getId() != null) return false;
+        if (getAuthor() != null ? !getAuthor().equals(book.getAuthor()) : book.getAuthor() != null) return false;
+        if (getLaunchDate() != null ? !getLaunchDate().equals(book.getLaunchDate()) : book.getLaunchDate() != null)
+            return false;
+        if (getTitle() != null ? !getTitle().equals(book.getTitle()) : book.getTitle() != null) return false;
+        if (getCurrency() != null ? !getCurrency().equals(book.getCurrency()) : book.getCurrency() != null)
+            return false;
+        return getEnvironment() != null ? getEnvironment().equals(book.getEnvironment()) : book.getEnvironment() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getAuthor() != null ? getAuthor().hashCode() : 0);
+        result = 31 * result + (getLaunchDate() != null ? getLaunchDate().hashCode() : 0);
+        temp = Double.doubleToLongBits(getPrice());
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (getTitle() != null ? getTitle().hashCode() : 0);
+        result = 31 * result + (getCurrency() != null ? getCurrency().hashCode() : 0);
+        result = 31 * result + (getEnvironment() != null ? getEnvironment().hashCode() : 0);
+        return result;
+    }
+
+}
---
+++ 05-cambio-service/src/main/java/br/com/erudio/cambioservice/controller/CambioController.java
@@ -0,0 +1,51 @@
+package br.com.erudio.cambioservice.controller;
+
+import br.com.erudio.cambioservice.model.Cambio;
+import br.com.erudio.cambioservice.repository.CambioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@RestController
+@RequestMapping("cambio-service")
+public class CambioController {
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private CambioRepository repository;
+
+    public Cambio cambioMet(String from, String to){
+        return repository.findByFromAndTo(from, to);
+    }
+
+    @GetMapping(value = "/{amount}/{from}/{to}")
+    public Cambio getCambio(
+            @PathVariable("amount") BigDecimal amount,
+            @PathVariable("from") String from,
+            @PathVariable("to") String to
+    ) {
+
+        var cambio = repository.findByFromAndTo(from, to);
+        cambio.getFrom();
+        //var cambio = new Cambio(from, to);
+
+        if (cambio == null) throw new RuntimeException("Currency Unsupported");
+
+        var port = environment.getProperty("local.server.port");
+        BigDecimal conversionFactor = cambio.getConversionFactor();
+        BigDecimal convertedValue = conversionFactor.multiply(amount);
+        cambio.setConvertedValue(convertedValue.setScale(2, RoundingMode.CEILING));
+        cambio.setEnvironment(port);
+        return cambio;
+    }
+}
+
+
+
